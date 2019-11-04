9dc1852771396df79edf81ad3cc7a26a5c6e476b
Evgeniy Lyashov <lyaev31@gmail.com>
2019-11-04 14:56:36 +0300

    go!!!

--- src/main/java/com/example/spring/GreetingController.java
+++ src/main/java/com/example/spring/GreetingController.java
@@ -14,4 +14,10 @@ public class GreetingController {
         return "greeting";
     }
 
+    @GetMapping("/")
+    public String greetingMain(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+
 }
