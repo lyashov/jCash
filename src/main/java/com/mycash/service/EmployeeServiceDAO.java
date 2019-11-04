d45e653ef401aa7076cd991e64503fc27872f73f
Evgeniy Lyashov <lyaev31@gmail.com>
2019-11-04 23:47:21 +0300

    sql1

--- src/main/java/com/mycash/service/EmployeeServiceDAO.java
+++ src/main/java/com/mycash/service/EmployeeServiceDAO.java
@@ -1,9 +1,4 @@
-package com.serviceDAO;
-
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
+package com.mycash.service;
 
 import com.mycash.exception.RecordNotFoundException;
 import com.mycash.model.EmployeeEntity;
@@ -11,9 +6,13 @@ import com.mycash.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeServiceDAO {
-
+     
     @Autowired
     EmployeeRepository repository;
 
@@ -33,50 +32,50 @@ public class EmployeeServiceDAO {
     public List<EmployeeEntity> getAllEmployees()
     {
         List<EmployeeEntity> employeeList = repository.findAll();
-
+         
         if(employeeList.size() > 0) {
             return employeeList;
         } else {
             return new ArrayList<EmployeeEntity>();
         }
     }
-
+     
     public EmployeeEntity getEmployeeById(Long id) throws RecordNotFoundException
     {
         Optional<EmployeeEntity> employee = repository.findById(id);
-
+         
         if(employee.isPresent()) {
             return employee.get();
         } else {
             throw new RecordNotFoundException("No employee record exist for given id");
         }
     }
-
+     
     public EmployeeEntity createOrUpdateEmployee(EmployeeEntity entity) throws RecordNotFoundException
     {
         Optional<EmployeeEntity> employee = repository.findById(entity.getId());
-
+         
         if(employee.isPresent())
         {
             EmployeeEntity newEntity = employee.get();
             newEntity.setEmail(entity.getEmail());
             newEntity.setFirstName(entity.getFirstName());
             newEntity.setLastName(entity.getLastName());
-
+ 
             newEntity = repository.save(newEntity);
-
+             
             return newEntity;
         } else {
             entity = repository.save(entity);
-
+             
             return entity;
         }
     }
-
+     
     public void deleteEmployeeById(Long id) throws RecordNotFoundException
     {
         Optional<EmployeeEntity> employee = repository.findById(id);
-
+         
         if(employee.isPresent())
         {
             repository.deleteById(id);
