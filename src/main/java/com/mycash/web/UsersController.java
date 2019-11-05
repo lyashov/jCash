9cc0a58c12c8cd79c5c10db12d8f9ad691b8c7ab
Evgeniy Lyashov <lyaev31@gmail.com>
2019-11-05 13:06:18 +0300

    users add2

--- src/main/java/com/mycash/web/UsersController.java
+++ src/main/java/com/mycash/web/UsersController.java
@@ -1,51 +1,17 @@
 package com.mycash.web;
 
-import com.mycash.exception.RecordNotFoundException;
-import com.mycash.model.UsersEntity;
-import com.mycash.service.UsersServiceDAO;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.List;
+@Controller
+public class UsersController {
 
-@RestController
+    @GetMapping("/hello")
+    public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "index";
+    }
 
-@RequestMapping("/users")
-public class UsersController
-{
-    @Autowired
-    UsersServiceDAO service;
- 
-    @GetMapping
-    public ResponseEntity<List<UsersEntity>> getAllEmployees() {
-        List<UsersEntity> list = service.getAllEmployees();
- 
-        return new ResponseEntity<List<UsersEntity>>(list, new HttpHeaders(), HttpStatus.OK);
-    }
- 
-    @GetMapping("/{id}")
-    public ResponseEntity<UsersEntity> getEmployeeById(@PathVariable("id") Long id)
-                                                    throws RecordNotFoundException {
-        UsersEntity entity = service.getEmployeeById(id);
- 
-        return new ResponseEntity<UsersEntity>(entity, new HttpHeaders(), HttpStatus.OK);
-    }
- 
-    @PostMapping
-    public ResponseEntity<UsersEntity> createOrUpdateEmployee(UsersEntity employee)
-                                                    throws RecordNotFoundException {
-        UsersEntity updated = service.createOrUpdateEmployee(employee);
-        return new ResponseEntity<UsersEntity>(updated, new HttpHeaders(), HttpStatus.OK);
-    }
- 
-    @DeleteMapping("/{id}")
-    public HttpStatus deleteEmployeeById(@PathVariable("id") Long id)
-                                                    throws RecordNotFoundException {
-        service.deleteEmployeeById(id);
-        return HttpStatus.FORBIDDEN;
-    }
- 
 }
