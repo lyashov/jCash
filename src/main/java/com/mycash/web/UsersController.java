1236b7e916e84a1bf2afdd6f4a2056f2998719b3
Evgeniy Lyashov <lyaev31@gmail.com>
2019-11-05 13:07:46 +0300

    users add3

--- src/main/java/com/mycash/web/UsersController.java
+++ src/main/java/com/mycash/web/UsersController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class UsersController {
 
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
         model.addAttribute("name", name);
         return "index";
